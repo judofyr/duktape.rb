baaf65a611003d189abeaaa9d073b30e2699b2cd
Magnus Holm <judofyr@gmail.com>
2014-01-14 21:07:40 +0100

    Group all tests using #describe

--- test/test_duktape.rb
+++ test/test_duktape.rb
@@ -10,28 +10,30 @@ class TestDuktape < Minitest::Spec
     @ctx = Duktape::Context.new
   end
 
-  def test_string
-    assert_equal '123', @ctx.eval_string('"123"', __FILE__)
-  end
+  describe "#eval_string" do
+    def test_string
+      assert_equal '123', @ctx.eval_string('"123"', __FILE__)
+    end
 
-  def test_boolean
-    assert_equal true,  @ctx.eval_string('1 == 1', __FILE__)
-    assert_equal false, @ctx.eval_string('1 == 2', __FILE__)
-  end
+    def test_boolean
+      assert_equal true,  @ctx.eval_string('1 == 1', __FILE__)
+      assert_equal false, @ctx.eval_string('1 == 2', __FILE__)
+    end
 
-  def test_number
-    assert_equal 123.0, @ctx.eval_string('123', __FILE__)
-  end
+    def test_number
+      assert_equal 123.0, @ctx.eval_string('123', __FILE__)
+    end
 
-  def test_nil_undef
-    assert_nil @ctx.eval_string('null', __FILE__)
-    assert_nil @ctx.eval_string('undefined', __FILE__)
-  end
+    def test_nil_undef
+      assert_nil @ctx.eval_string('null', __FILE__)
+      assert_nil @ctx.eval_string('undefined', __FILE__)
+    end
 
 
-  def test_complex_error
-    assert_raises(Duktape::ContextError) do
-      @ctx.eval_string('a = {b:1}', __FILE__)
+    def test_complex_error
+      assert_raises(Duktape::ContextError) do
+        @ctx.eval_string('a = {b:1}', __FILE__)
+      end
     end
   end
 
@@ -47,17 +49,19 @@ class TestDuktape < Minitest::Spec
     end
   end
 
-  def test_get_prop
-    @ctx.eval_string('a = 1', __FILE__)
-    assert_equal 1.0, @ctx.get_prop('a')
-  end
-
-  def test_get_prop_failed
-    err = assert_raises(Duktape::ContextError) do
-      @ctx.get_prop('a')
+  describe "#get_prop" do
+    def test_basic
+      @ctx.eval_string('a = 1', __FILE__)
+      assert_equal 1.0, @ctx.get_prop('a')
     end
 
-    assert_equal 'no such prop: a', err.message
+    def test_missing
+      err = assert_raises(Duktape::ContextError) do
+        @ctx.get_prop('a')
+      end
+
+      assert_equal 'no such prop: a', err.message
+    end
   end
 
   describe "#call_prop" do
